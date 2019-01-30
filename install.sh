d39f5fc4f9c86c5dec335e1bc844c157a6479b34
常乐 <Leptune@users.noreply.github.com>
2019-01-30 16:04:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd ~
 rm -rf .path_manage_leptune
 git clone https://github.com/Leptune/path_manage.git .path_manage_leptune --depth=1
 cd .path_manage_leptune
-cat .bashrc >> ~/.bashrc
+echo 'source ~/.path_manage_leptune/.bashrc' >> ~/.bashrc
 source ~/.bashrc 
 echo 'success~ enjoy it~~'
 
