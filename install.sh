baa7e2eb7cee050cf09e5012c56fdb3142cdc756
常乐 <Leptune@users.noreply.github.com>
2019-01-30 16:23:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,17 @@ git clone https://github.com/Leptune/path_manage.git .path_manage_leptune --dept
 cd .path_manage_leptune
 echo 'source ~/.path_manage_leptune/bashrc' >> ~/.bashrc
 source ~/.bashrc 
-echo '安装成功！！欢迎使用~~'
 echo '
-# Linux路径管理系统~
+
+--------------------------------------------------------
+恭喜您，安装成功！！
+# 欢迎使用Linux路径管理系统~~
 # 命令列表
+>- `pmh`     : 显示帮助信息
 >- `pma 别名`: 将当前路径添加到别名
 >- `pml`     : 别名对应的实际路径列表
 >- `pmg 别名`: 跳转到别名所对应的路径
 >- `pmd 别名`: 删除该别名
->- `pmh`     : 显示帮助信息
 >- `pmr 旧别名 新别名`: 重命名
+---------------------------------------------------------
 '
